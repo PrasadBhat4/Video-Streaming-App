8a69d1424ff044482e50213b362f09f49af05035
PrasadBhat4 <40450109+PrasadBhat4@users.noreply.github.com>
2019-11-13 14:56:18 +0530

    hello

---
+++ client/src/components/Modal.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const Modal = props => {
+  return ReactDOM.createPortal(
+    <div className="ui dimmer modals visible acive">
+      <div className="ui standard modal visible active">kdhiosdh</div>
+    </div>
+  );
+};
+export default Modal;
